01ae6ec6f6103cf7a6b1d9c2b28d36a812a1678e
Teodor Ivanov <teodori@aiidatapro.com>
2018-08-15 10:11:19 +0300

    Refactor FeedFinder class

--- feedfinder2.py
+++ feedfinder2.py
@@ -66,92 +66,98 @@ class FeedFinder(object):
         return any(map(url.lower().count,
                        ["rss", "rdf", "xml", "atom", "feed"]))
 
+    def sort_urls(self, feeds):
+        return sorted(list(set(feeds)), key=self.url_feed_prob, reverse=True)
+
+    def url_feed_prob(self, url):
+        if "comments" in url:
+            return -2
+        if "georss" in url:
+            return -1
+        kw = ["atom", "rss", "rdf", ".xml", "feed"]
+        for p, t in zip(range(len(kw), 0, -1), kw):
+            if t in url:
+                return p
+        return 0
+
+    def find_feeds(self, url, check_all=False):
+        # Format the URL properly.
+        url = coerce_url(url)
+
+        # Download the requested URL.
+        text = self.get_feed(url)
+        if text is None:
+            return []
+
+        # Check if it is already a feed.
+        if self.is_feed_data(text):
+            return [url]
+
+        # Look for <link> tags.
+        logging.info("Looking for <link> tags.")
+        tree = BeautifulSoup(text, "html.parser")
+        links = []
+        for link in tree.find_all("link"):
+            if link.get("type") in ["application/rss+xml",
+                                    "text/xml",
+                                    "application/atom+xml",
+                                    "application/x.atom+xml",
+                                    "application/x-atom+xml"]:
+                links.append(urlparse.urljoin(url, link.get("href", "")))
+
+        # Check the detected links.
+        urls = list(filter(self.is_feed, links))
+        logging.info("Found {0} feed <link> tags.".format(len(urls)))
+        if urls and not check_all:
+            return self.sort_urls(urls)
+
+        # Look for <a> tags.
+        logging.info("Looking for <a> tags.")
+        local, remote = [], []
+        for a in tree.find_all("a"):
+            href = a.get("href", None)
+            if href is None:
+                continue
+            if "://" not in href and self.is_feed_url(href):
+                local.append(href)
+            if self.is_feedlike_url(href):
+                remote.append(href)
+
+        # Check the local URLs.
+        local = [urlparse.urljoin(url, l) for l in local]
+        urls += list(filter(self.is_feed, local))
+        logging.info("Found {0} local <a> links to feeds.".format(len(urls)))
+        if len(urls) and not check_all:
+            return self.sort_urls(urls)
+
+        # Check the remote URLs.
+        remote = [urlparse.urljoin(url, l) for l in remote]
+        urls += list(filter(self.is_feed, remote))
+        logging.info("Found {0} remote <a> links to feeds.".format(len(urls)))
+        if len(urls) and not check_all:
+            return self.sort_urls(urls)
+
+        # Guessing potential URLs.
+        fns = ["atom.xml", "index.atom", "index.rdf", "rss.xml", "index.xml",
+               "index.rss"]
+        urls += list(filter(self.is_feed, [urlparse.urljoin(url, f)
+                                             for f in fns]))
+        return self.sort_urls(urls)
+
+
+# class FeedValidator(object):
+#
+
+
+
 
 def find_feeds(url, check_all=False, user_agent=None, timeout=None):
     finder = FeedFinder(user_agent=user_agent, timeout=timeout)
-
-    # Format the URL properly.
-    url = coerce_url(url)
-
-    # Download the requested URL.
-    text = finder.get_feed(url)
-    if text is None:
-        return []
-
-    # Check if it is already a feed.
-    if finder.is_feed_data(text):
-        return [url]
-
-    # Look for <link> tags.
-    logging.info("Looking for <link> tags.")
-    tree = BeautifulSoup(text, "html.parser")
-    links = []
-    for link in tree.find_all("link"):
-        if link.get("type") in ["application/rss+xml",
-                                "text/xml",
-                                "application/atom+xml",
-                                "application/x.atom+xml",
-                                "application/x-atom+xml"]:
-            links.append(urlparse.urljoin(url, link.get("href", "")))
-
-    # Check the detected links.
-    urls = list(filter(finder.is_feed, links))
-    logging.info("Found {0} feed <link> tags.".format(len(urls)))
-    if len(urls) and not check_all:
-        return sort_urls(urls)
-
-    # Look for <a> tags.
-    logging.info("Looking for <a> tags.")
-    local, remote = [], []
-    for a in tree.find_all("a"):
-        href = a.get("href", None)
-        if href is None:
-            continue
-        if "://" not in href and finder.is_feed_url(href):
-            local.append(href)
-        if finder.is_feedlike_url(href):
-            remote.append(href)
-
-    # Check the local URLs.
-    local = [urlparse.urljoin(url, l) for l in local]
-    urls += list(filter(finder.is_feed, local))
-    logging.info("Found {0} local <a> links to feeds.".format(len(urls)))
-    if len(urls) and not check_all:
-        return sort_urls(urls)
-
-    # Check the remote URLs.
-    remote = [urlparse.urljoin(url, l) for l in remote]
-    urls += list(filter(finder.is_feed, remote))
-    logging.info("Found {0} remote <a> links to feeds.".format(len(urls)))
-    if len(urls) and not check_all:
-        return sort_urls(urls)
-
-    # Guessing potential URLs.
-    fns = ["atom.xml", "index.atom", "index.rdf", "rss.xml", "index.xml",
-           "index.rss"]
-    urls += list(filter(finder.is_feed, [urlparse.urljoin(url, f)
-                                         for f in fns]))
-    return sort_urls(urls)
-
-
-def url_feed_prob(url):
-    if "comments" in url:
-        return -2
-    if "georss" in url:
-        return -1
-    kw = ["atom", "rss", "rdf", ".xml", "feed"]
-    for p, t in zip(range(len(kw), 0, -1), kw):
-        if t in url:
-            return p
-    return 0
-
-
-def sort_urls(feeds):
-    return sorted(list(set(feeds)), key=url_feed_prob, reverse=True)
+    return finder.find_feeds(url, check_all)
 
 
 if __name__ == "__main__":
-    print(find_feeds("www.preposterousuniverse.com/blog/", timeout = 1))
+    print(find_feeds("www.preposterousuniverse.com/blog/", timeout=1))
     print(find_feeds("www.preposterousuniverse.com/blog/"))
     print(find_feeds("http://xkcd.com"))
     print(find_feeds("dan.iel.fm/atom.xml"))
@@ -159,4 +165,3 @@ if __name__ == "__main__":
     print(find_feeds("kapadia.github.io"))
     print(find_feeds("blog.jonathansick.ca"))
     print(find_feeds("asdasd"))
-    
