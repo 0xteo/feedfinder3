c638a38f493d5e8d8f21d52009cf51e9f6223cc4
Teodor Ivanov <tdrivanov@gmail.com>
2018-08-15 13:54:09 +0300

    Update feedfinder2 -> feedfinder3

---
+++ feedfinder3.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+__version__ = "0.0.5"
+
+try:
+    __FEEDFINDER2_SETUP__
+except NameError:
+    __FEEDFINDER2_SETUP__ = False
+
+if not __FEEDFINDER2_SETUP__:
+    __all__ = ["find_feeds"]
+
+    from components.finders import (
+        FeedFinder,
+    )
+    from components.validators import (
+        FeedValidator,
+    )
+
+
+def find_feeds(url,
+               check_all=False,
+               user_agent=None,
+               timeout=None,
+               validate_options=None):
+    finder = FeedFinder(user_agent=user_agent, timeout=timeout)
+    feeds = finder.find_feeds(url, check_all)
+    if validate_options:
+        feeds = [
+            feed for feed in feeds
+            if FeedValidator(feed, **validate_options).is_valid
+        ]
+    return feeds
+
+
+if __name__ == "__main__":
+    print(find_feeds("www.preposterousuniverse.com/blog/", timeout=1))
+    print(find_feeds("www.preposterousuniverse.com/blog/"))
+    print(find_feeds("http://xkcd.com",
+                     validate_options={
+                         'max_day_interval': 1
+                     }
+                     )
+          )
+    print(find_feeds("dan.iel.fm/atom.xml"))
+    print(find_feeds("dan.iel.fm", check_all=True))
+    print(find_feeds("kapadia.github.io"))
+    print(find_feeds("blog.jonathansick.ca"))
+    print(find_feeds("asdasd"))
